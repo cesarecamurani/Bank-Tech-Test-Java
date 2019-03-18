e5353417d2a1cad9acf78cfcf6ad2d2ce3071254
Cesare Camurani <cesare.camurani@gmail.com>
2019-03-18 20:01:53 +0000

    Date needs to be mocked in tests

--- src/main/bank/Transaction.java
+++ src/main/bank/Transaction.java
@@ -1,27 +1,30 @@
 package bank;
 
 import java.util.*;
-import java.time.ZonedDateTime;
+import java.time.*;
+import java.time.format.*;
 
 class Transaction{
 
     private TreeMap transactions;
+    private ZonedDateTime rawDate = ZonedDateTime.now();
+    private String date = DateTimeFormatter.ofPattern("dd/MM/YYYY").format(rawDate);
 
     Transaction(TreeMap transactions){
         this.transactions = transactions;
     }
 
     void credit(Integer amount, Integer balance){
-        this.transactions.put("date", ZonedDateTime.now());
-        this.transactions.put("credit", amount);
-        this.transactions.put("debit", null);
-        this.transactions.put("balance", balance);
+        this.transactions.put("date", date);
+        this.transactions.put("credit", amount.toString());
+        this.transactions.put("debit", "0");
+        this.transactions.put("balance", balance.toString());
     }
 
     void debit(Integer amount, Integer balance){
-        this.transactions.put("date", ZonedDateTime.now());
-        this.transactions.put("credit", null);
-        this.transactions.put("debit", amount);
-        this.transactions.put("balance", balance);
+        this.transactions.put("date", date);
+        this.transactions.put("credit", "0");
+        this.transactions.put("debit", amount.toString());
+        this.transactions.put("balance", balance.toString());
     }
 }
--- src/test/bank/TransactionTest.java
+++ src/test/bank/TransactionTest.java
@@ -2,6 +2,8 @@ package bank;
 
 import org.junit.*;
 import java.util.*;
+import java.time.*;
+import java.time.format.*;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
@@ -9,8 +11,20 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TransactionTest{
 
+    private Instant rawDate = Instant.now(Clock.fixed(
+            Instant.parse("2018-08-22T10:00:00Z"),
+            ZoneOffset.UTC));
+
+    private DateTimeFormatter formatter =
+            DateTimeFormatter.ofPattern("dd/MM/YYYY")
+                    .withLocale( Locale.UK )
+                    .withZone( ZoneId.systemDefault());
+
+    private String fixedDate = formatter.format(rawDate);
+
+
     private Transaction transaction;
-    private TreeMap<String, Integer> transactions;
+    private TreeMap<String, String> transactions;
 
     @Before
     public void setup(){
@@ -21,22 +35,33 @@ public class TransactionTest{
     @Test
     public void creditShouldAddCreditToStatementEx1() {
         transaction.credit(1000, 1000);
-        assertTrue(transactions.get("credit").equals(1000));
-        assertTrue(transactions.get("balance").equals(1000));
+        System.out.println(fixedDate);
+        System.out.println(transactions.get("date"));
+        assertTrue(transactions.get("date").equals(fixedDate));
+        assertTrue(transactions.get("credit").equals("1000"));
+        assertTrue(transactions.get("balance").equals("1000"));
     }
 
     @Test
     public void creditShouldAddCreditToStatementEx2() {
         transaction.credit(2000, 2000);
-        assertTrue(transactions.get("credit").equals(2000));
-        assertTrue(transactions.get("balance").equals(2000));
+        assertTrue(transactions.get("credit").equals("2000"));
+        assertTrue(transactions.get("balance").equals("2000"));
     }
 
     @Test
     public void debitShouldAddDebitToStatementEx1() {
         transaction.credit(1000, 1000);
         transaction.debit(800, 200);
-        assertTrue(transactions.get("debit").equals(800));
-        assertTrue(transactions.get("balance").equals(200));
+        assertTrue(transactions.get("debit").equals("800"));
+        assertTrue(transactions.get("balance").equals("200"));
+    }
+
+    @Test
+    public void debitShouldAddDebitToStatementEx2() {
+        transaction.credit(2000, 2000);
+        transaction.debit(1500, 500);
+        assertTrue(transactions.get("debit").equals("1500"));
+        assertTrue(transactions.get("balance").equals("500"));
     }
 }
