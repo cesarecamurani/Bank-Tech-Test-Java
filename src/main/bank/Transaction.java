242a3069e56329aeb2f5e6d8a9d0675e650e4a3d
Cesare Camurani <cesare.camurani@gmail.com>
2019-03-19 15:43:40 +0000

    Huge refactoring Transaction tests - transactions changed to List<Map>

--- src/main/bank/Transaction.java
+++ src/main/bank/Transaction.java
@@ -1,43 +1,36 @@
 package bank;
 
 import java.util.*;
-import java.time.*;
-import java.time.format.*;
 
 interface DateTime {
     Date getDate();
 }
 
-class DateTimeImpl implements DateTime {
-    @Override
-    public Date getDate() {
-        return new Date();
-    }
-}
-
 class Transaction{
 
-    DateTime dateTime;
-    private TreeMap transactions;
-    private ZonedDateTime rawDate = ZonedDateTime.now();
-    private String date = DateTimeFormatter.ofPattern("dd/MM/YYYY").format(rawDate);
+    private DateTime dateTime;
+    private Map<String,String> transactionCre = new HashMap<>();
+    private Map<String,String> transactionDeb = new HashMap<>();
+    private List<Map<String , String>> transactions;
 
-    Transaction(TreeMap transactions, DateTime dateTime){
+    Transaction(List<Map<String , String>> transactions, DateTime dateTime){
         this.transactions = transactions;
         this.dateTime = dateTime;
     }
 
     void credit(Integer amount, Integer balance){
-        this.transactions.put("date", this.dateTime.getDate().toString());
-        this.transactions.put("credit", amount.toString());
-        this.transactions.put("debit", "0");
-        this.transactions.put("balance", balance.toString());
+        this.transactionCre.put("date", this.dateTime.getDate().toString());
+        this.transactionCre.put("credit", amount.toString());
+        this.transactionCre.put("debit", "0");
+        this.transactionCre.put("balance", balance.toString());
+        this.transactions.add(0, transactionCre);
     }
 
     void debit(Integer amount, Integer balance){
-        this.transactions.put("date", date);
-        this.transactions.put("credit", "0");
-        this.transactions.put("debit", amount.toString());
-        this.transactions.put("balance", balance.toString());
+        this.transactionDeb.put("date", this.dateTime.getDate().toString());
+        this.transactionDeb.put("credit", "0");
+        this.transactionDeb.put("debit", amount.toString());
+        this.transactionDeb.put("balance", balance.toString());
+        this.transactions.add(0, transactionDeb);
     }
 }
--- src/test/bank/TransactionTest.java
+++ src/test/bank/TransactionTest.java
@@ -4,31 +4,15 @@ import org.junit.*;
 import org.mockito.Mockito;
 
 import java.util.*;
-import java.time.*;
-import java.time.format.*;
-import static org.mockito.Mockito.*;
-import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.hamcrest.collection.IsMapContaining;
 
 public class TransactionTest{
 
-//    private Instant rawDate = Instant.now(Clock.fixed(
-//            Instant.parse("2018-08-22T10:00:00Z"),
-//            ZoneOffset.UTC));
-//
-//    private DateTimeFormatter formatter =
-//            DateTimeFormatter.ofPattern("dd/MM/YYYY")
-//                    .withLocale( Locale.UK )
-//                    .withZone( ZoneId.systemDefault());
-//
-//    private String fixedDate = formatter.format(rawDate);
-
-
     private Transaction transaction;
-    private TreeMap<String, String> transactions;
-    Date date;
-    DateTime dt;
+    private List<Map<String, String>> transactions;
+    private Date date;
+    private DateTime dt;
 
     @Before
     public void setup(){
@@ -41,39 +25,45 @@ public class TransactionTest{
         Mockito.when(dt.getDate())
                 .thenReturn(date);
 
-        transactions = new TreeMap<>();
+        transactions = new ArrayList<>();
         transaction = new Transaction(transactions, dt);
-
     }
 
     @Test
     public void creditShouldAddCreditToStatementEx1() {
         transaction.credit(1000, 1000);
-        assertTrue(transactions.get("date").equals(date.toString()));
-        assertTrue(transactions.get("credit").equals("1000"));
-        assertTrue(transactions.get("balance").equals("1000"));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("date", "20/08/2018"));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("credit", "1000"));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("balance", "1000"));
     }
 
     @Test
     public void creditShouldAddCreditToStatementEx2() {
         transaction.credit(2000, 2000);
-        assertTrue(transactions.get("credit").equals("2000"));
-        assertTrue(transactions.get("balance").equals("2000"));
+        System.out.println(transactions.get(0));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("date", "20/08/2018"));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("credit", "2000"));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("balance", "2000"));
     }
 
     @Test
     public void debitShouldAddDebitToStatementEx1() {
         transaction.credit(1000, 1000);
         transaction.debit(800, 200);
-        assertTrue(transactions.get("debit").equals("800"));
-        assertTrue(transactions.get("balance").equals("200"));
+        System.out.println(transactions.get(0));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("date", "20/08/2018"));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("debit", "800"));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("balance", "200"));
     }
 
     @Test
     public void debitShouldAddDebitToStatementEx2() {
         transaction.credit(2000, 2000);
         transaction.debit(1500, 500);
-        assertTrue(transactions.get("debit").equals("1500"));
-        assertTrue(transactions.get("balance").equals("500"));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("date", "20/08/2018"));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("debit", "1500"));
+        assertThat(transactions.get(0), IsMapContaining.hasEntry("balance", "500"));
     }
 }
+
+
